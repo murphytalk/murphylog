ec0aa2ab86f0ae4b2d0a2d7bfddb2c4a46bd795b
murphy <murphy@LinkStation.(none)>
2009-07-25 02:30:59 +0900

    extension to docutils/syntax highlighting

---
+++ murphytalk_django/murphymarkup/templatetags/enhanced_docutils.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+定义了一个新的directive: sh 和一个新的writer： myhtml
+
+"""
+
+#========================================================================================
+# 自定义detective: 语法高亮
+# Directive Type:	"sh" (SyntaxHighlighting)
+# Doctree Element:	literal_block
+# Directive Arguments:	one
+# Directive Options:	None.
+# Directive Content:	create a literal_block with option : {"syntax":"language"}
+from docutils import nodes
+from docutils.parsers.rst import directives
+
+def syntax_highlighting(name, arguments, options, content, lineno,
+                        content_offset, block_text, state, state_machine):
+    if not content:
+        warning = state_machine.reporter.warning(
+            'Content expected for the "%s" directive; none found.'
+            % name, nodes.literal_block(block_text, block_text), line=lineno)
+        return [warning]
+    text = '\n'.join(content)
+    node = nodes.literal_block(text, text)
+
+    node.option = {"syntax":arguments[0]} #check if this option is availble to distinguish with
+                                          #normal literal block directive
+
+    node.line = content_offset + 1
+    return [node]
+
+#parsed_literal.options = {'class': directives.class_option}
+syntax_highlighting.content = True         #有content
+syntax_highlighting.arguments = (1, 0, 1)  #(required_arguments,optional_arguments,final_argument_whitespace)
+
+#注册
+directives.register_directive("sh",syntax_highlighting)
+#========================================================================================
+
+#========================================================================================
+# 自定义writer: 继承自 html4css1
+# 可以识别自定义directive: sh的结果
+from docutils.writers.html4css1 import Writer,HTMLTranslator
+from docutils import writers
+
+class MyHtmlWriter(Writer):
+    def __init__(self):
+        writers.Writer.__init__(self)
+        #super(MyHtmlWriter,self).__init__(self)
+        self.translator_class = MyHTMLTranslator
+
+class MyHTMLTranslator(HTMLTranslator):
+    def visit_literal_block(self, node):
+        if hasattr(node,'option') and node.option.has_key('syntax'):
+            self.body.append(self.starttag(node, 'pre', NAME='code',CLASS=node.option['syntax']))
+        else:
+            HTMLTranslator.visit_literal_block(self,node)
+
+import docutils.writers
+
+#patch the "writer register"
+original_get_writer_class = docutils.writers.get_writer_class
+
+def patched_get_writer_class(writer_name):
+    if writer_name == "myhtml":
+        return MyHtmlWriter
+    else:
+        return original_get_writer_class(writer_name)
+
+docutils.writers.get_writer_class = patched_get_writer_class
+#========================================================================================
+
+if __name__ == "__main__":
+    src="""
+my docutils enhancement.
+
+normal literal block::
+
+ i am literal block
+
+END of literal block.
+
+html raw:
+
+.. raw:: html
+
+ <font size=1>test</font>
+
+END of raw
+
+syntax highlight:
+
+.. sh:: cpp
+
+   void CController::call(int idx,BASES& objs)
+   {
+       for(BASES::iterator pos=objs.begin();pos!=objs.end();++pos){
+           ((*pos)->*(m_funcs[idx-1]))();
+       }
+   }
+
+
+END of syntax highlight
+    """
+    from docutils.core import publish_parts
+
+    parts = publish_parts(source=src, writer_name="myhtml",settings_overrides={'raw_enabled':False})
+    print parts["fragment"]
